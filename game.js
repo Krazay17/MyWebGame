32636587d8c8da0f519536822f2266479e870eb7
Krazay17 <Joshmass7@gmail.com>
2025-05-15 02:00:06 -0400

    moving platforms

--- game.js
+++ game.js
@@ -14,6 +14,10 @@ var config = {
         debug: false,
       },
     },
+    // fps: {
+    //   target: 60,
+    //   forceSetTimeOut: true,
+    // },
     scene: {
         preload: preload,
         create: create,
@@ -24,7 +28,13 @@ var config = {
 var game = new Phaser.Game(config);
 
 let player;
+let stunned = false;
+let stunHandle;
+let playerSpeed = 200;
+
 let coins;
+let totalCoins;
+let platforms;
 let bullets;
 let score = 0;
 let scoreText;
@@ -48,26 +58,32 @@ function create() {
   this.add.image(600, 300, "sky").setScale(.3);
 
   // Add a static platform
-  const platforms = this.physics.add.staticGroup();
-  platforms.create(100, 700, "platform").setScale(1).refreshBody();
-  platforms.create(300, 700, "platform").setScale(1).refreshBody();
-  platforms.create(500, 700, "platform").setScale(1).refreshBody();
-  platforms.create(700, 700, "platform").setScale(1).refreshBody();
-  platforms.create(900, 600, "platform").setScale(1).refreshBody();
-  platforms.create(1100, 600, "platform").setScale(1).refreshBody();
-  platforms.create(1300, 500, "platform").setScale(1).refreshBody();
-  platforms.create(1100, 400, "platform").setScale(1).refreshBody();
-  platforms.create(900, 300, "platform").setScale(1).refreshBody();
-  platforms.create(700, 200, "platform").setScale(1).refreshBody();
-  platforms.create(350, 400, "platform").setScale(1).refreshBody();
+  platforms = this.physics.add.group({allowGravity: false, immovable: true});
+
+  const platform1 = platforms.create(100, 700, "platform").setScale(1);
+  const platform2 = platforms.create(300, 700, "platform").setScale(1);
+  const platform3 = platforms.create(500, 700, "platform").setScale(1);
+  const platform4 = platforms.create(700, 700, "platform").setScale(1);
+  const platform5 = platforms.create(900, 600, "platform").setScale(1);
+  const platform6 = platforms.create(1100, 600, "platform").setScale(1);
+  const platform7 = platforms.create(1300, 500, "platform").setScale(1);
+  const platform8 = platforms.create(1100, 400, "platform").setScale(1);
+  const platform9 = platforms.create(900, 300, "platform").setScale(1);
+  const platform10 = platforms.create(700, 200, "platform").setScale(1);
+
+  platforms.children.iterate(function (platform){
+    platform.body.setVelocityX(-50);
+  });
 
   // Add Coins
-  coins = this.physics.add.group();
+  coins = this.physics.add.group({collideWorldBounds: true});
   coins.create(300, 500, 'coin').setScale(.2).setBounce(1);
-  coins.create(500, 350, 'coin').setScale(.2).setBounce(1);
-  coins.create(700, 400, 'coin').setScale(.2).setBounce(1);
-  coins.create(350, 100, 'coin').setScale(.2).setBounce(1);
+  coins.create(500, 500, 'coin').setScale(.2).setBounce(1);
+  coins.create(700, 500, 'coin').setScale(.2).setBounce(1);
+  coins.create(900, 500, 'coin').setScale(.2).setBounce(1);
+  coins.create(1100, 500, 'coin').setScale(.2).setBounce(1);
   this.physics.add.collider(coins, platforms);
+  totalCoins = coins.countActive(true);
 
   bullets = this.physics.add.group();
   SpawnBullets(10);
@@ -83,7 +99,7 @@ function create() {
   player.setOffset(55, 5);
 
   // Add physics collider with player
-  this.physics.add.collider(player, platforms);
+  this.physics.add.collider(player, platforms, CarryPlayer, null, this);
   this.physics.add.overlap(player, coins, collectCoin, null, this);
   this.physics.add.overlap(player, bullets, bulletHit, null, this);
 
@@ -99,25 +115,29 @@ function create() {
 
 }
 
+// Tick Function
 /** @this {Phaser.Scene} */
 function update() {
 
-  if (cursors.left.isDown) {
-    player.setVelocityX(-200);
-  } else if (cursors.right.isDown) {
-    player.setVelocityX(200);
-  } else {
-    player.setVelocityX(0);
-  }
+  MovementInput();
 
-  if ((cursors.up.isDown || cursors.space.isDown )&& player.body.touching.down) {
-    player.setVelocityY(-400);
+  if (player.y > config.height){
+    loseGame.call(this);
   }
 
   coins.children.iterate(function (coin){
     coin.rotation += 0.05;
   });
+
+  platforms.children.iterate(function (platform) {
+  if (platform.x + (platform.displayWidth / 2) < 0) {
+    platform.x = config.width + (platform.displayWidth / 2);
+  }
+});
+
+
 }
+
 
 function collectCoin(player, coin)
 {
@@ -127,12 +147,9 @@ function collectCoin(player, coin)
   scoreText.setText('Source: ' + score);
 
   this.sound.play('pickup');
-  if (score == 4)
+  if (score === totalCoins)
   {
-    winText = this.add.text(config.width/2, config.height/2, 'YOU WIN!', {
-      fontSize: '132px',
-      color: '#4fffff'
-  });
+    WinGame.call(this);
   }
 }
 
@@ -141,6 +158,12 @@ function bulletHit(player, bullets)
   bullets.destroy();
   player.setVelocityY(-400);
   player.setVelocityX(-400);
+
+  stunned = true;
+  setTimeout(() =>{
+    stunned = false;
+}, 500);
+
   this.sound.play('playerdamage');
 }
 
@@ -154,11 +177,66 @@ function SpawnBullets(amount)
   for (let i = 0; i < amount; i++) {
     const bullet = bullets.create(
     1400, 
-    Phaser.Math.Between(config.height/2, config.height), 
+    Phaser.Math.Between(config.height/2, config.height-100),
     'bullet'
     );
 
     bullet.body.setAllowGravity(false);
     bullet.setVelocityX((Phaser.Math.Between(50, 300) * -1));
   }
+}
+
+//A function that moves the player with the platform
+function CarryPlayer(localplayer, localplatform)
+{
+  //localplayer.x += localplatform.body.velocity.x * (this.game.loop.delta);
+  player.body.setVelocityX(localplatform.body.velocity.x);
+}
+
+function Lerp(start, end, amount) 
+{
+  return start + (end - start) * amount;
+}
+
+function WalkLerp(a) 
+{
+  const modify = player.body.touching.down ? .4 : .025;
+  
+  return Lerp(player.body.velocity.x, a, modify);
+}
+
+function MovementInput()
+{
+  if (stunned) return;
+  if (cursors.left.isDown) {
+    player.setVelocityX(WalkLerp(-200));
+  } else if (cursors.right.isDown) {
+    player.setVelocityX(WalkLerp(200));
+  } else if (player.body.velocity.x > 0) {
+    player.setVelocityX(WalkLerp(0));
+  }
+    if ((cursors.up.isDown || cursors.space.isDown ) && player.body.touching.down) {
+    player.setVelocityY(-400);
+  }
+}
+
+/** @this {Phaser.Scene} */
+function loseGame() 
+{
+  this.physics?.pause(); // Stop physics
+  player.setTint(0xff0000); // Flash red
+  player.anims?.stop?.(); // Stop any animations
+  const gameOverText = this.add.text(config.width / 2, config.height / 2, 'YOU LOSE', {
+    fontSize: '64px',
+    color: '#ff0000'
+  });
+  gameOverText.setOrigin(0.5);
+}
+
+function WinGame()
+{
+    winText = this.add.text(config.width/2, config.height/2, 'YOU WIN!', {
+      fontSize: '132px',
+      color: '#4fffff'
+  });
 }

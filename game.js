7097eec4783d5e83832a809e836b77e3c95adb1c
Krazay17 <Joshmass7@gmail.com>
2025-05-16 01:51:03 -0400

    some update

--- game.js
+++ game.js
@@ -5,8 +5,8 @@
 
 var config = {
     type: Phaser.AUTO,
-    width: 1400,
-    height: 800,
+    width: 1600,
+    height: 900,
     physics: {
       default: "arcade",
       arcade: {
@@ -28,17 +28,22 @@ var config = {
 var game = new Phaser.Game(config);
 
 let restartKey;
+let gameMusic;
 
 let player;
 let dashKey;
 let canJump;
+let canDash = true;
 let wallJump = false;
 let wallJumpX;
 let jumpPower = 200;
 let jumpTimer;
 let stunned = false;
 let stunHandle;
-let playerSpeed = 200;
+let stunTimer;
+let playerSpeed = 250;
+let hitSound;
+let pickupSound;
 
 let coins;
 let totalCoins;
@@ -55,6 +60,7 @@ let scoreText;
 let winText;
 let cursors;
 
+
 /** @this {Phaser.Scene} */
 function preload() {
   this.load.image("sky", 'Assets/RedGalaxy2.png');
@@ -68,14 +74,22 @@ function preload() {
   this.load.image('fireBall', 'Assets/Fireball.png');
   this.load.audio('pickup', 'Assets/SuccessBeep.wav');
   this.load.audio('playerdamage', 'Assets/PlayerGotHit.wav');
+  this.load.audio('music', 'Assets/Music.wav');
 
 }
 
 /** @this {Phaser.Scene} */
 function create() {
-  this.add.image(600, 300, "sky").setScale(.3);
+  this.add.image(600, 300, "sky").setScale(.3).setScrollFactor(0);
   restartKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
   dashKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SHIFT)
+  hitSound = this.sound.add('playerdamage');
+  pickupSound = this.sound.add('pickup');
+  if (!this.sound.get('music')){
+    gameMusic = this.sound.add('music', {loop: true});
+    gameMusic.play();
+  }
+
   // Add a static platform
   platforms = this.physics.add.group({allowGravity: false, immovable: true});
 
@@ -92,11 +106,12 @@ function create() {
   const platform11 = platforms.create(400, 550, "platform").setScale(1);
   const platform12 = platforms.create(100, 550, "platform").setScale(1);
   const platform13 = platforms.create(900, 600, "platform").setScale(1);
+  const platform14 = platforms.create(300, 400, "platform").setScale(1);
 
-  platforms.children.iterate(function (platform){
-    platform.body.setVelocityX(-50);
-    return undefined;
-  });
+  // platforms.children.iterate(function (platform){
+  //   platform.body.setVelocityX(-50);
+  //   return undefined;
+  // });
 
   // Add Coins
   coins = this.physics.add.group({collideWorldBounds: true});
@@ -115,7 +130,8 @@ function create() {
   }, 5000);
 
   sunMan = this.physics.add.group({collideWorldBounds: true});
-  sunMan.create(1111, 500, 'sunMan').setBounce(1).setVelocityX(-50);
+  sunMan.create(1000, 300, 'sunMan').setBounce(1).setVelocityX(-100);
+  sunMan.create(1200, 300, 'sunMan').setBounce(1).setVelocityX(-150);
 
   turretPlat = this.physics.add.group({allowGravity: false, collideWorldBounds: true, immovable: true});
   turretPlat1 = turretPlat.create(1200, 600, 'turretPlat').setVelocityY(-50).setBounce(1);
@@ -131,13 +147,15 @@ function create() {
   player.setScale(.3)
   player.setSize(105, 240);
   player.setOffset(55, 5);
+  this.cameras.main.startFollow(player, false, .01, .01);
 
   // Add physics collider with player
   this.physics.add.collider(player, platforms, CarryPlayer, null, this);
   this.physics.add.collider(player, turretPlat, CarryPlayer, null, this);
   this.physics.add.overlap(player, coins, collectCoin, null, this);
   this.physics.add.overlap(player, bullets, bulletHit, null, this);
-  this.physics.add.overlap(player, sunMan, bulletHit, null, this);
+  this.physics.add.collider(player, sunMan, SunHit, null, this);
+  this.physics.add.collider(sunMan, sunMan);
   this.physics.add.overlap(player, fireBalls, bulletHit, null, this);
 
   // Add cursor input
@@ -150,6 +168,10 @@ function create() {
   });
   scoreText.setScrollFactor(0);
 
+  this.input.on('pointerdown', function (pointer){
+    console.log('MouseClickedat:', pointer.x, pointer.y);
+  });
+
 }
 
 // Tick Function
@@ -158,6 +180,7 @@ function update() {
   if (restartKey.isDown){
     RestartGame.call(this);
   }
+
   MovementInput();
 
   if (player.y > config.height){
@@ -185,25 +208,45 @@ function collectCoin(player, coin)
   score ++;
   scoreText.setText('Source: ' + score);
 
-  this.sound.play('pickup');
+  if (pickupSound && pickupSound.isPlaying)
+    pickupSound.stop();
+  pickupSound.play();
+
   if (score === totalCoins)
   {
     WinGame.call(this);
   }
 }
 
-function bulletHit(player, bullets)
+function bulletHit(player, bullet)
 {
-  bullets.destroy();
-  player.setVelocityY(-400);
-  player.setVelocityX(-400);
+  player.setVelocityY(-200);
+  player.setVelocityX(bullet.body.velocity.x);
+  bullet.destroy();
 
   stunned = true;
-  setTimeout(() =>{
+  stunTimer = setTimeout(() =>{
     stunned = false;
 }, 500);
 
-  this.sound.play('playerdamage');
+  if (hitSound && hitSound.isPlaying)
+    hitSound.stop();
+  hitSound.play();
+}
+
+function SunHit(player, bullet)
+{
+  player.setVelocityY(-200);
+  player.setVelocityX(bullet.body.velocity.x);
+
+  stunned = true;
+  stunTimer = setTimeout(() =>{
+    stunned = false;
+}, 500);
+
+  if (hitSound && hitSound.isPlaying)
+    hitSound.stop();
+  hitSound.play();
 }
 
 function getRandomInt(min, max)
@@ -239,23 +282,30 @@ function Lerp(start, end, amount)
 
 function WalkLerp(a) 
 {
-  const modify = player.body.touching.down ? .4 : .04;
+  const modify = player.body.touching.down ? .5 : .04;
   
   return Lerp(player.body.velocity.x, a, modify);
 }
 
-/** @this {Phaser.GameObjects.Sprite} */
 function MovementInput()
 {
   if (stunned) return;
   if (cursors.left.isDown && cursors.down.isUp) {
-    player.setVelocityX(WalkLerp(-200));
+    player.setVelocityX(WalkLerp(-playerSpeed));
     player.flipX = true;
-    if (dashKey.isDown) player.setVelocityX(-400);
+    if (dashKey.isDown && canDash){ 
+      player.setVelocityX(-500); 
+      player.setVelocityY(-50); 
+      canDash = false;
+    }
   } else if (cursors.right.isDown && cursors.down.isUp) {
-    player.setVelocityX(WalkLerp(200));
+    player.setVelocityX(WalkLerp(playerSpeed));
     player.flipX = false;
-    if (dashKey.isDown) player.setVelocityX(400);
+    if (dashKey.isDown && canDash) { 
+      player.setVelocityX(500); 
+      player.setVelocityY(-50); 
+      canDash = false;
+    }
   } else if (player.body.velocity.x > 0) {
     player.setVelocityX(WalkLerp(0));
   }
@@ -295,6 +345,7 @@ function ResetJump()
   canJump = true;
   jumpPower = 200;
   wallJump = false;
+  canDash = true;
   clearTimeout(jumpTimer);
 }
 
@@ -339,4 +390,8 @@ function FireballSpawn(amount)
     fireBall.body.setAllowGravity(false);
     fireBall.setVelocityX(-444);
   }
+}
+
+function PlayerShoot()
+{
 }

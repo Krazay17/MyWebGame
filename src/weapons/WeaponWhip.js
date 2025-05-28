8f8f51ee39328f345100fdf51ad8ff0e2bb0cbef
Krazay17 <Joshmass7@gmail.com>
2025-05-27 22:50:17 -0400

    whip fix 4

--- src/weapons/WeaponWhip.js
+++ src/weapons/WeaponWhip.js
@@ -2,32 +2,30 @@ import WeaponBase from './_weaponbase.js';
 
 export default class WeaponWhip extends WeaponBase {
     constructor(scene, player) {
-        super(scene, player, 1);
+        super(scene, player, 1)
 
-        this.name = 'whip';
+        this.name = 'whip'
         this.baseCooldown = 500;
         this.meleeDuration = 250;
         this.spamAdd = 100;
         this.hitSoundId = 'energysound';
-
-        this.isConnected = false;
+        this.whipConnect = false;
         this.wasGrappling = false;
         this.rayTrackEnd = false;
-        this.hitLocation = null;
-        this.grappleSpeed = 3000;
-        this.isHolding = false;
 
-        this.createAnimations(scene);
-    }
-
-    createAnimations(scene) {
         if (!scene.anims.get('whip')) {
             scene.anims.create({
                 key: 'whip',
                 defaultTextureKey: 'whip',
                 duration: 150,
-                frames: scene.anims.generateFrameNumbers('whip', { start: 0, end: 4 })
-            });
+                frames: [
+                    { frame: 0 },
+                    { frame: 1 },
+                    { frame: 2 },
+                    { frame: 3 },
+                    { frame: 4 },
+                ],
+            })
         }
 
         if (!scene.anims.get('whipend')) {
@@ -35,162 +33,142 @@ export default class WeaponWhip extends WeaponBase {
                 key: 'whipend',
                 defaultTextureKey: 'whip',
                 duration: 100,
-                frames: scene.anims.generateFrameNumbers('whip', { start: 4, end: 0 }),
-                hideOnComplete: true
-            });
+                frames: [
+                    { frame: 4 },
+                    { frame: 3 },
+                    { frame: 2 },
+                    { frame: 1 },
+                    { frame: 0 },
+                ],
+                hideOnComplete: true,
+            })
         }
     }
-
     update(delta) {
         super.update(delta);
-        if (!this.weaponSprite) return;
+        if (this.weaponSprite) {
+            this.weaponSprite.x = this.player.x;
+            this.weaponSprite.y = this.player.y - 25 + this.weaponSpriteOffset.y;
+        }
 
-        this.weaponSprite.setPosition(
-            this.player.x,
-            this.player.y - 25 + this.weaponSpriteOffset.y
-        );
-
-        if (this.weaponSprite && this.isConnected && this.hitLocation) {
+        if (this.weaponSprite && this.whipConnect && this.hitLocation && this.holding) {
+            console.log(this.hitLocation)
             this.wasGrappling = true;
             this.rayTrackEnd = true;
-
+            // const g = this.scene.add.graphics();
+            // g.lineStyle(4, 0xffffff, 1);
+            // g.beginPath();
+            // g.moveTo(this.player.x, this.player.y);
+            // g.lineTo(this.hitLocation.x, this.hitLocation.y);
+            // g.strokePath();
             const distance = Phaser.Math.Distance.Between(
                 this.player.x, this.player.y,
                 this.hitLocation.x, this.hitLocation.y
             );
-
             this.rayTickData.end.x = this.hitLocation.x;
             this.rayTickData.end.y = this.hitLocation.y;
+            const angleDeg = Phaser.Math.RadToDeg(Phaser.Math.Angle.Between(this.weaponSprite.x, this.weaponSprite.y, this.hitLocation.x, this.hitLocation.y));
 
-            const angleDeg = Phaser.Math.RadToDeg(
-                Phaser.Math.Angle.Between(
-                    this.weaponSprite.x, this.weaponSprite.y,
-                    this.hitLocation.x, this.hitLocation.y
-                )
-            );
-
-            if (distance > 100) {
-                this.scene.physics.accelerateTo(
-                    this.player,
-                    this.hitLocation.x,
-                    this.hitLocation.y,
-                    this.grappleSpeed,
-                    450,
-                    450
-                );
-            }
-
+            if (distance > 100) this.scene.physics.accelerateTo(this.player, this.hitLocation.x, this.hitLocation.y, 3000, 450, 450);
             this.weaponSprite.setAngle(angleDeg);
-            this.weaponSprite.setScale(this.mapRangeClamped(distance, 25, 400, 0.02, 0.6));
-
-            if (distance > 600) {
-                this.weaponSprite.play('whipend');
-                this.isConnected = false;
+            this.weaponSprite.setScale(this.mapRangeClamped(distance, 25, 400, 0.02, 0.6))
+            if (distance >600 && this.weaponSprite) {
+                this.weaponSprite.play('whipend')
+                this.whipConnect = false
             }
-        } else if (this.wasGrappling) {
+        } 
+        else if (this.wasGrappling) {
             this.player.body.setAcceleration(0, 0);
             this.player.body.setMaxSpeed(1200);
             this.player.body.maxVelocity.x = 1200;
             this.player.body.maxVelocity.y = 1200;
-
             this.rayTrackEnd = false;
-            this.isConnected = false;
+            this.whipConnect = false;
             this.meleeRayTick = false;
-
             if (this.weaponSprite) {
                 this.weaponSprite.play('whipend', true);
             }
-
             this.wasGrappling = false;
         }
     }
 
     fire(pointer) {
-        if (!this.canFire() || this.weaponSprite) return;
-
+        if (this.weaponSprite) return;
+        if (!this.canFire()) return;
         this.clearHits();
         this.playThrowSound();
-        this.isHolding = true;
 
         const data = this.calculateShot(pointer, 15);
-        const angle = Phaser.Math.RadToDeg(
-            Phaser.Math.Angle.Between(
-                data.start.x,
-                data.start.y,
-                data.cursorPos.x,
-                data.cursorPos.y
-            )
-        );
+        const angleDeg = Phaser.Math.RadToDeg(Phaser.Math.Angle.Between(data.start.x, data.start.y, data.cursorPos.x, data.cursorPos.y));
 
         this.weaponSpriteOffset = data.vector;
 
-        this.weaponSprite = this.scene.add.sprite(
-            data.start.x + data.vector.x,
-            data.start.y + data.vector.y,
-            'whip'
-        )
+        this.weaponSprite = this.scene.add.sprite(data.start.x + data.vector.x, data.start.y + data.vector.y, 'whip')
             .setScale(0.48)
-            .setOrigin(0, 0.5)
-            .setAngle(angle)
-            .setFlipY(angle > 90 || angle < -90)
-            .play('whip');
+            .setOrigin(0, .5)
+            .setAngle(angleDeg);
+        this.weaponSprite.play('whip');
+        this.weaponSprite.setFlipY(angleDeg > 90 || angleDeg < -90);
 
-        // Delay ray trace until halfway through animation
-        this.scene.time.delayedCall(25, () => {
-            if (this.isHolding) {
-                this.meleeRayTick = true;
-                console.log('tracing?')
-                this.rayTickData = this.calculateShot(pointer, 235);
-            }
+        const rayData = this.calculateShot(pointer, 235);
+        this.rayTickData = rayData;
+        this.holding = true;
+        // this.weaponSprite.on('animationcomplete-whip', () => {
+        //     this.meleeRayTick = true;
+        // });
+
+        this.scene.time.delayedCall(50, () => this.meleeRayTick = true)
+
+        this.weaponSprite.on('animationcomplete-whipend', () => {
+            this.stopWhip();
         });
-
-        this.weaponSprite.on('animationcomplete-whip', () => this.meleeRayTick = true);
-        this.weaponSprite.on('animationcomplete-whipend', () => this.stopWhip());
 
         this.player.on('playerstunned', () => this.release());
     }
 
-    platformHit(_, hitLocation) {
-        this.connect(hitLocation);
-    }
-
-    itemHit(_, hitLocation) {
-        this.connect(hitLocation);
-    }
-
-    connect(hitLocation) {
-        if (!hitLocation || this.isConnected || !this.isHolding) return;
-
-        this.hitLocation = hitLocation;
-        this.isConnected = true;
-
-        if (this.weaponSprite) {
-            this.weaponSprite.stop();
-            this.weaponSprite.setFrame(4);
-        }
-    }
-
     release() {
-        this.isHolding = false;
-        this.isConnected = false;
-
-        if (this.weaponSprite) {
+        this.whipConnect = false;
+        this.holding = false;
+        this.meleeRayTick = false;
+        if (this.weaponSprite && this.weaponSprite.anims.isPlaying) {
+            this.weaponSprite.chain('whipend');
+        } else if (this.weaponSprite){
             this.weaponSprite.play('whipend');
         }
     }
 
+    platformHit(plat, hitLocation) {
+        if (this.whipConnect && hitLocation) return;
+        this.hitLocation = hitLocation;
+        this.whipConnect = true;
+        if (this.weaponSprite) {
+            this.weaponSprite.stop();
+            this.weaponSprite.setFrame(4)
+        }
+    }
+
+    itemHit(item, hitLocation) {
+        if (this.whipConnect && hitLocation) return;
+        this.hitLocation = hitLocation;
+        this.whipConnect = true;
+        if (this.weaponSprite) {
+            this.weaponSprite.stop();
+            this.weaponSprite.setFrame(4)
+        }
+    }
+
+    connect(hitRect) {
+
+    }
+
     stopWhip() {
         this.startCooldown();
-        this.isConnected = false;
+        this.whipConnect = false;
         this.meleeRayTick = false;
-        this.hitLocation = null;
-        this.isHolding = false;
-
         if (this.weaponSprite) {
-            this.weaponSprite.off('animationcomplete-whip');
-            this.weaponSprite.off('animationcomplete-whipend');
+            this.weaponSprite.stop();
             this.weaponSprite.destroy();
-            this.weaponSprite = null;
+            delete this.weaponSprite;
         }
     }
 }
